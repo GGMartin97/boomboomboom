4946b22eb4a73e217dc9b7cc0475c72840776bb2
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-12-27 16:13:36 +0800

    read

--- pythonboom.py
+++ pythonboom.py
@@ -29,20 +29,20 @@ REG_PART_ID         = 0XFF
 
 
 def maxim_max30102_reset():
-	bus.write_byte(REG_MODE_CONFIG,0X40)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_MODE_CONFIG,0X40)
 
 def maxim_max30102_init():
-	bus.write_byte(REG_INTR_ENABLE_1,0xc0)
-	bus.write_byte(REG_INTR_ENABLE_2,0x00)
-	bus.write_byte(REG_FIFO_WR_PTR,0x00)
-	bus.write_byte(REG_OVF_COUNTER,0x00)
-	bus.write_byte(REG_FIFO_RD_PTR,0x00)
-	bus.write_byte(REG_FIFO_CONFIG,0x0f)
-	bus.write_byte(REG_MODE_CONFIG,0x03)
-	bus.write_byte(REG_SPO2_CONFIG,0x27)
-	bus.write_byte(REG_LED1_PA,0x24)
-	bus.write_byte(REG_LED2_PA,0x24)
-	bus.write_byte(REG_PILOT_PA,0x7f)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_INTR_ENABLE_1,0xc0)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_INTR_ENABLE_2,0x00)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_FIFO_WR_PTR,0x00)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_OVF_COUNTER,0x00)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_FIFO_RD_PTR,0x00)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_FIFO_CONFIG,0x0f)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_MODE_CONFIG,0x03)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_SPO2_CONFIG,0x27)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_LED1_PA,0x24)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_LED2_PA,0x24)
+	bus.write_byte_data(MAX30102_I2CADDR,REG_PILOT_PA,0x7f)
 
 
 
