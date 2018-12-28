6a041cd83da3a2a2f2ac9de35e99648ab51372c7
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-12-28 10:12:55 +0800

    1

--- pythonboom.py
+++ pythonboom.py
@@ -1,6 +1,7 @@
 import time
 import smbus
 
+
 #MAX30102 address
 MAX30102_I2CADDR = 0X57
 
@@ -28,10 +29,10 @@ REG_PART_ID         = 0XFF
 
 
 
-def maxim_max30102_reset():
+def maxim_max30102_reset():   
 	bus.write_byte_data(MAX30102_I2CADDR,REG_MODE_CONFIG,0X40)
 
-def maxim_max30102_init():
+def maxim_max30102_init():    
 	bus.write_byte_data(MAX30102_I2CADDR,REG_INTR_ENABLE_1,0xc0)
 	bus.write_byte_data(MAX30102_I2CADDR,REG_INTR_ENABLE_2,0x00)
 	bus.write_byte_data(MAX30102_I2CADDR,REG_FIFO_WR_PTR,0x00)
@@ -44,13 +45,48 @@ def maxim_max30102_init():
 	bus.write_byte_data(MAX30102_I2CADDR,REG_LED2_PA,0x24)
 	bus.write_byte_data(MAX30102_I2CADDR,REG_PILOT_PA,0x7f)
 
+def maxim_max30102_read_fifo():
+	temp_red_led = 0
+	temp_ir_led  = 0
+	ach_i2c_data=[]
+	uch_temp=bus.read_byte_data(MAX30102_I2CADDR,REG_INTR_STATUS_1)
+	uch_temp=bus.read_byte_data(MAX30102_I2CADDR,REG_INTR_STATUS_2)
+	bus.write_byte(MAX30102_I2CADDR,REG_FIFO_DATA)
+	time.sleep(0.001)
+	print('hello')
+	for i in range(0,6):
+		ach_i2c_data.append(bus.read_byte_data(MAX30102_I2CADDR,REG_FIFO_DATA))
+	un_temp=ach_i2c_data[0]<<16
+	temp_red_led=temp_red_led+un_temp
+	un_temp=ach_i2c_data[1]<<8
+	temp_red_led=temp_red_led+un_temp
+	un_temp=ach_i2c_data[2]
+	temp_red_led=temp_red_led+un_temp
+
+	un_temp=ach_i2c_data[3]<<16
+	temp_ir_led=temp_ir_led+un_temp
+	un_temp=ach_i2c_data[4]<<8
+	temp_ir_led=temp_ir_led+un_temp
+	un_temp=ach_i2c_data[5]
+	temp_ir_led=temp_ir_led+un_temp
+	return temp_red_led,temp_ir_led
+
+
+
+
 
 
 if __name__=="__main__":
 	address=MAX30102_I2CADDR
 	bus=smbus.SMBus(1)
 	maxim_max30102_reset()
-	wait(0.5)
+	time.sleep(0.5)
 	maxim_max30102_init()
-	wait(0.5)
-
+	time.sleep(0.5)
+	martin1=[]
+	martin2=[]
+	for h in range(0,500):
+		h1,h2=maxim_max30102_read_fifo()
+		martin1.append(h1)
+		martin2.append(h2)
+	print(martin1)
